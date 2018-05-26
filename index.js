86920665f5f4e130ea6be08e1bb81944cd06eabb
Cheryl Kelson <cherylkelson@icloud.com>
2018-05-26 20:57:24 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,28 @@
+function shout(string) {
+  return string.toUpperCase();
+}
+function whisper(string) {
+   return string.toLowerCase();
+}
+function logShout(string) {
+  console.log(string.toUpperCase());
+}
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
+function sayHiToGrandma(string) {
+  console.log("STRING: " + string)
+  if (string.toLowerCase()===string) {
+    return("I can't hear you!");
+  }
+  if (string.toUpperCase()===string){
+    return("YES INDEED!")
+  }
+  if("I love you, Grandma"===string){
+    return("I love you, too.");
+  }
+  if(string==undefined)
+  {
+    return("I love you, too.");
+  }
+}
